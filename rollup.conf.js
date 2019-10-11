5bc45f814a9bc799c206da7de5db98845dc30286
Dmitriy Trifonov <trifonov.dmitriy.m@gmail.com>
2019-10-11 17:32:14 +0700

    Add button to augment each item on demand Disable automatic augmentation of all items

--- rollup.conf.js
+++ rollup.conf.js
@@ -2,7 +2,7 @@ let bannerText = `
 // ==UserScript==
 // @name         DTF: Expand feed items
 // @namespace    http://tampermonkey.net/
-// @version      0.3.1
+// @version      0.4.0
 // @description  Expand feed items!
 // @author       mr-m
 // @match        *://dtf.ru/*
--- src/main.js
+++ src/main.js
@@ -122,19 +122,39 @@ function toChunks (arr, chunkSize) {
   return result;
 }
 
-async function augmentFeedItems (items) {
-  for (let chunk of toChunks(items, 3)) {
-    const promises = chunk.map(function (feedItem) {
-      return augmentWithContent(feedItem)
-        .catch(function (reason) { return reason; });
-    });
-    await Promise.all(promises);
+function augmentFeedItems (items) {
+  items.forEach(async function (feedItem) {
+    let html = `
+      <a class="content-header-number content-header__item">
+        <div class="content-header-author__avatar">
+          <svg class="icon icon--favorite_marker" width="18" height="18" viewBox="0 0 24 24" style="fill:none;stroke-width:2;stroke:currentColor">
+            <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
+            <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
+          </svg>
+        </div>
+        <div class="content-header-author__name">Весь текст</div>
+      </a>
+    `;
 
-    //Apparently Air modules are run before augmentation fulfills.
-    //Here, we force the modules to run again after the augmentation.
-    window.Air.get("module.quiz").refresh();
-    window.Air.get("module.andropov").refresh();
-  }
+    let aElement = await Promise.resolve(html)
+      .then(str => new DOMParser().parseFromString(str, 'text/html'))
+      .then(doc => doc.body.firstElementChild);
+
+    aElement.addEventListener('click', async function (event) {
+      event.preventDefault();
+
+      let feedItem = event.target.closest('.feed__item');
+      await augmentWithContent(feedItem);
+
+      //Apparently Air modules are run before augmentation fulfills.
+      //Here, we force the modules to run again after the augmentation.
+      window.Air.get("module.quiz").refresh();
+      window.Air.get("module.andropov").refresh();
+    });
+
+    let headerLeft = feedItem.querySelector('.content-header__left');
+    headerLeft.appendChild(aElement);
+  });
 }
 
 async function augmentWithContent (item) {
